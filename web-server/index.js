c6f0dc0e886f584644af1d82c1f97a1e6c7594d2
Hijikata1869 <liv.knk@gmail.com>
2023-03-22 20:34:40 +0900

    完成

--- web-server/index.js
+++ web-server/index.js
@@ -1,9 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
 
-app.get('/', function (req, res) {
-  console.log(req);
-  res.send('<h1>トップページ!!!!!</h1>')
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")))
+
+// app.get('/', function (req, res) {
+//   console.log(req);
+//   res.send('<h1>トップページ!!!!!</h1>')
+// })
+
+app.post('/api/v1/quiz', function (req, res) {
+  const answer = req.body.answer;
+  if(answer === "2") {
+    // res.send("正解！")
+    res.redirect("/correct.html")
+  } else {
+    // res.send("不正解！")
+    res.redirect("/wrong.html")
+  }
 })
 
 app.get('/api/v1/users', function (req, res) {
