db7dc1ec06ec1cdea425222ba30edbe7b78f4a14
Hijikata1869 <liv.knk@gmail.com>
2023-03-22 18:59:27 +0900

    first commit

---
+++ web-server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  console.log(req);
+  res.send('<h1>トップページ!!!!!</h1>')
+})
+
+app.get('/api/v1/users', function (req, res) {
+  res.send({
+    name: "Mike",
+    age: 30,
+  })
+})
+
+app.listen(8000, function () {
+  console.log("I am runnnig");
+});
